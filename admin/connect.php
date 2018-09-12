32af33a435bdda9b9461ca97547c2766f06d3994
Cheks <okekechekwube1@gmail.com>
2018-09-12 07:29:46 +0100

    Delete connect.php

--- admin/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host = "localhost";
-$db = "nid";
-$username = "root";
-$password = "";
-
-$conn = mysqli_connect($host,$username,$password,$db) or die("DB not connected");
-$query = mysqli_select_db($conn, $db);
